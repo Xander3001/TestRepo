9ee99b72e16538b3f889d631cb50845618f68828
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-02-12 12:51:45 +0000

    Adding code comments

--- Testfolder/Digit_Recognizer_DL.py
+++ Testfolder/Digit_Recognizer_DL.py
@@ -33,3 +33,44 @@ def relu_backward(dA, cache):
     return dZ
 
 
+
+# # import numpy and matplotlib
+# import numpy as np
+# import matplotlib.pyplot as plt
+# 
+# # Implementing Relu function
+# 
+# 
+# def softmax(z):
+#     # Keeping only negative values
+#     cache = z
+#     z -= np.max(z)
+#     # Rounding to 16 decimal points
+#     sm = (np.exp(z).T / np.sum(np.exp(z), axis=1))
+#     return sm, cache
+# 
+# 
+# def relu(z):
+# 
+#     s = np.maximum(0, z)
+#     # Keeping only negative values
+#     cache = z
+#     return s, cache
+# 
+# 
+# def softmax_backward(dA, cache):
+# 
+#     # Keeping only negative values
+#     z = cache
+#     z -= np.max(z)
+#     s = (np.exp(z).T / np.sum(np.exp(z), axis=1))
+#     dZ = dA * s * (1 - s)
+#     return dZ
+# 
+# 
+# def relu_backward(dA, cache):
+#     # Keep only positive zeros 
+#     Z = cache
+#     dZ = np.array(dA, copy=True)  # just converting dz to a correct object.
+#     dZ[Z <= 0] = 0
+#     return dZ
