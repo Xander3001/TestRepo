3235a4eb8a69a6c11b7d0e6fc129991bb6d62c64
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-02-12 12:02:33 +0000

    Add files via upload

---
+++ TestFolder2/RedditScraping&DataCleaning.py
@@ -0,0 +1,31 @@
+
+for post in ml_subreddit.hot(limit=10000):
+    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+
+
+posts.replace('', np.nan, inplace=True)
+posts.dropna(inplace=True)
+
+
+def  clean_text(df, text_field, new_text_field_name):
+    df[new_text_field_name] = df[text_field].str.lower()
+    df[new_text_field_name] = df[new_text_field_name].apply(lambda elem: re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", elem))  
+    # remove numbers
+    df[new_text_field_name] = df[new_text_field_name].apply(lambda elem: re.sub(r"\d+", "", elem))
+    
+    return df
+data_cleans = clean_text(posts, 'title', 'clean_title')
+
+data_cleans.head()
+
+
+data_cleans['clean_text'] = data_cleans['clean_body'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+
+data_cleans['text_tokens'] = data_cleans['clean_text'].apply(lambda x: word_tokenize(x))
+
+
+data_clean = data_cleans.drop(['title', "body","text_tokens", "subreddit", "url", "clean_title", "clean_body"], axis=1) 
+data_clean.dropna(inplace=True)
+
+
