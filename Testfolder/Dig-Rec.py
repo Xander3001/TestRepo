2ca3f14422b908b6aa34e95d614b89fce1891e45
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-02-13 06:44:43 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -5,3 +5,12 @@ def fib(num):
         return fib(num-1) + fib(num-2)
 print(fib(10))
 
+
+# # creates the fibnocci series and prints it
+# 
+# def fib(num):
+#     if num <= 1:
+#         return num
+#     else:
+#         return fib(num-1) + fib(num-2)
+# print(fib(10))
