942a1f598b5eb4c2c4977b757c7a5802561f2c81
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-02-14 11:07:36 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -5,3 +5,16 @@ def fib(num):
         return fib(num-1) + fib(num-2)
 print(fib(10))
 
+
+# '''
+# Recursive function that calculates the fibonaci series up to num: 14
+# it calulates by calling itself when n=0 by n-1 then n-2 whenever a condition is met as it regresses upwards 
+# '''
+# 
+# def fib(num):
+#     if num <= 1:
+#         return num
+#     else:
+#         return fib(num-1) + fib(num-2)
+#         return(num)
+# print(fib(14))
