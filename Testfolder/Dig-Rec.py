396938c6999309e73b7bdca7212d0accfa472b16
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-02-15 13:46:48 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -5,3 +5,10 @@ def fib(num):
         return fib(num-1) + fib(num-2)
 print(fib(10))
 
+
+def fib(num):
+    if num <= 1:  # For n equal to 0 or 1
+        return num
+    else:
+        return fib(num-1) + fib(num-2) # For n greater than 1 i.e 2 or more
+print(fib(10))
