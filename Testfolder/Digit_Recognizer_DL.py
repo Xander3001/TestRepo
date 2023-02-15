f616b34963b9e2b3a29a0b865e57ce65923758d3
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-02-15 09:26:33 +0000

    Adding code comments

--- Testfolder/Digit_Recognizer_DL.py
+++ Testfolder/Digit_Recognizer_DL.py
@@ -33,3 +33,60 @@ def relu_backward(dA, cache):
     return dZ
 
 
+
+def initialize_parameters(n_x, n_h, n_y):
+    np.random.seed(1)
+    W1 = np.random.randn(n_h, n_x) * 0.01
+    b1 = np.zeros(shape=(n_h, 1))
+    W2 = np.random.randn(n_y, n_h) * 0.01
+    b2 = np.zeros(shape=(n_y, 1))
+    assert (W1.shape == (n_h, n_x))
+    assert (b1.shape == (n_h, 1))
+    assert (W2.shape == (n_y, n_h))
+    assert (b2.shape == (n_y, 1))
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    return parameters
+
+
+# math sign
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+
+    # set cache to the input z, for computing the backward pass
+def softmax(z):
+    # Compute the forward pass
+    cache = z
+    #  Take the exponent(np.exp)
+    z -= np.max(z)
+    sm = (np.exp(z).T / np.sum(np.exp(z), axis=1))
+    return sm, cache
+
+
+
+def relu(z):
+
+    s = np.maximum(0, z)
+    cache = z
+    return s, cache
+
+
+def softmax_backward(dA, cache):
+
+    z = cache
+    z -= np.max(z)
+    s = (np.exp(z).T / np.sum(np.exp(z), axis=1))
+    dZ = dA * s * (1 - s)
+    return dZ
+
+
+def relu_backward(dA, cache):
+    Z = cache
+    dZ = np.array(dA, copy=True)  # just converting dz to a correct object.
+    dZ[Z <= 0] = 0
+    return dZ
