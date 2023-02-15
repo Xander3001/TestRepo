36a51055df7d583aad96142074c95f2ab32bee04
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-02-15 13:46:40 +0000

    Adding code comments

--- TestFolder2/Scraping&Cleaning.py
+++ TestFolder2/Scraping&Cleaning.py
@@ -29,3 +29,24 @@ data_clean = data_cleans.drop(['title', "body","text_tokens", "subreddit", "url"
 data_clean.dropna(inplace=True)
 
 
+
+## Scraping 10000 posts from r/machinelearning
+for post in ml_subreddit.hot(limit=10000):
+    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+## Removing empty rows/columns
+## Defining a function to remove URLs, step text and clean up the words
+posts.replace('', np.nan, inplace=True)
+posts.dropna(inplace=True)
+
+
+def  clean_text(df, text_field, new_text_field_name):
+    df[new_text_field_name] = df[text_field].str.lower()
+    df[new_text_field_name] = df[new_text_field_name].apply(lambda elem: re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", elem))  
+    # remove numbers
+    df[new_text_field_name] = df[new_text_field_name].apply(lambda elem: re.sub(r"\d+", "", elem))
+    
+    return df
+# Examples of the before and after.
+'Free hands-on Machine/Deep Learning, Python, Enterprise/Cloud Computing, Artificial Intelligence (AI)/Internet of Things (IoT) Programming and Data Science classes.'
+'free hands on machine deep learning python enterprise cloud computing artificial intelligence aiiot programming and data science classes'
