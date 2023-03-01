d61f1cb20e412c73fd3be1069c030d6a45fc6ec4
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-01 20:59:52 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -5,3 +5,23 @@ def fib(num):
         return fib(num-1) + fib(num-2)
 print(fib(10))
 
+
+def fib(num):
+    """
+    Returns the nth number in the Fibonacci sequence using recursion.
+
+    Parameters:
+    num (int): The position of the desired Fibonacci number in the sequence.
+
+    Returns:
+    The nth Fibonacci number.
+
+    """
+    if num <= 1:  # base case
+        return num
+    else:
+        # recursive case: adds up the previous two numbers in the sequence
+        return fib(num-1) + fib(num-2)
+
+# prints the 10th Fibonacci number
+print(fib(10))
