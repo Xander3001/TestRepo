95e10b33a796cc7b21ebdbf80f75345a4a10e89d
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-01 20:59:41 +0000

    Adding code comments

--- TestFolder2/RandomF.py
+++ TestFolder2/RandomF.py
@@ -70,3 +70,82 @@ for index, doc in enumerate(entities):
             full_df.at[index, entity_type] = entity
         else:
             full_df.at[index, entity_type] = full_df.at[index, entity_type] + ", {}".format(entity)
+
+# create a list of nouns from the input text by filtering out stop words, punctuation, and selecting only nouns
+nouns = [ token.text for token in doc if token.is_stop != True and token.is_punct !=True and token.pos_ == 'NOUN']
+
+# count the frequency of all the nouns and select the 10 most common ones
+word_freq = Counter(nouns)
+common_nouns = word_freq.most_common(10)
+
+# print the 10 most common nouns
+print(common_nouns)
+
+
+# create a list of verbs from the input text by filtering out punctuation and selecting only verbs
+verbs = [ token.text for token in doc if token.is_punct !=True and token.pos_ == 'VERB']
+
+# count the frequency of all the verbs and select the 10 most common ones
+print(Counter(verbs).most_common(10))
+
+
+# define a function that redacts names in a text by merging named entities and replacing person entities with [REDACTED]
+def sanitize_names(text):
+    doc = nlp(text)
+    redacted_sentences = []
+    for ent in doc.ents:
+        ent.merge()
+    for token in doc:
+        if token.ent_type_ == 'PERSON':
+            redacted_sentences.append("[REDACTED]")
+        else:
+            redacted_sentences.append(token.string)
+    return "".join(redacted_sentences)
+  
+
+# define a function that redacts locations in a text by merging named entities and replacing geography entities with [REDACTED]
+def sanitize_locations(text):
+    doc = nlp(text)
+    redacted_sentences = []
+    for ent in doc.ents:
+        ent.merge()
+    for token in doc:
+        if token.ent_type_ == 'GPE':
+            redacted_sentences.append("[REDACTED]")
+        else:
+            redacted_sentences.append(token.string)
+    return "".join(redacted_sentences)
+  
+  
+# define a function that tokenizes a sentence using Spacy parser, lemmatizes the words, removes stop words and punctuations, and returns the tokens as a string
+def spacy_tokenizer(sentence):
+    mytokens = parser(sentence)
+    mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
+    mytokens = [ word for word in mytokens if word not in stopwords and word not in punctuations ]
+    mytokens = " ".join([i for i in mytokens])
+    return mytokens
+
+
+# define a function that prints the top n topics from a document-term matrix using an NMF model and a vectorizer
+def selected_topics(model, vectorizer, top_n=10):
+    for idx, topic in enumerate(model.components_):
+        print("Topic %d:" % (idx))
+        print([(vectorizer.get_feature_names()[i], topic[i])
+                        for i in topic.argsort()[:-top_n - 1:-1]]) 
+
+# create a list of named entities in the input text by iterating over each text in a pandas dataframe using multiple threads
+entities = []
+for text in nlp.pipe(iter(bestdf['Description']), batch_size = 1000, n_threads=-1):  
+  entities.append([(X.text, X.label_) for X in text.ents])
+  print([(X.text, X.label_) for X in text.ents])
+
+# create a dataframe with columns for different named entity types and fill it with named entities extracted from the input text
+index=range(0,13911)
+full_df = pd.DataFrame(index = index, columns=['DOC NR','CARDINAL', 'ORDINAL', 'QUANTITY', 'MONEY', 'PERCENT', 'TIME', 'DATE', 'LANGUAGE', 'LAW', 'WORK_OF_ART', 'EVENT', 'PRODUCT', 'LOC', 'GPE', 'ORG', 'FAC', 'NORP', 'PERSON'])
+
+for index, doc in enumerate(entities):
+    for entity, entity_type in doc:
+        if pd.isna(full_df.at[index, entity_type]):
+            full_df.at[index, entity_type] = entity
+        else:
+            full_df.at[index, entity_type] = full_df.at[index, entity_type] + ", {}".format(entity)
