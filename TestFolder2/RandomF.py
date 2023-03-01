e1e7b750fe310d1f1d702d51f6dd5944d090e3a1
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-01 22:01:27 +0000

    Adding code comments

--- TestFolder2/RandomF.py
+++ TestFolder2/RandomF.py
@@ -1,15 +1,24 @@
+# This code extracts common nouns and verbs from a given text and redacts all personal names and locations
 
+# Import necessary libraries
+from collections import Counter
+import spacy
+import pandas as pd
 
-nouns = [ token.text for token in doc if token.is_stop != True and token.is_punct !=True and token.pos_ == 'NOUN']
+# Load Spacy NLP model
+nlp = spacy.load("en_core_web_sm")
+
+# Extract common nouns from the text
+nouns = [token.text for token in doc if token.is_stop != True and token.is_punct !=True and token.pos_ == 'NOUN']
 word_freq = Counter(nouns)
 common_nouns = word_freq.most_common(10)
 print(common_nouns)
 
-
-verbs = [ token.text for token in doc if token.is_punct !=True and token.pos_ == 'VERB']
+# Extract common verbs from the text
+verbs = [token.text for token in doc if token.is_punct !=True and token.pos_ == 'VERB']
 print(Counter(verbs).most_common(10))
 
-
+# Function to redact all personal names from the text
 def sanitize_names(text):
     doc = nlp(text)
     redacted_sentences = []
@@ -21,9 +30,8 @@ def sanitize_names(text):
         else:
             redacted_sentences.append(token.string)
     return "".join(redacted_sentences)
-  
-  
 
+# Function to redact all locations from the text
 def sanitize_locations(text):
     doc = nlp(text)
     redacted_sentences = []
@@ -35,35 +43,33 @@ def sanitize_locations(text):
         else:
             redacted_sentences.append(token.string)
     return "".join(redacted_sentences)
-  
-  
-  
-  
+
+# Function to tokenize a sentence using Spacy
 def spacy_tokenizer(sentence):
     mytokens = parser(sentence)
-    mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
-    mytokens = [ word for word in mytokens if word not in stopwords and word not in punctuations ]
+    mytokens = [word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens]
+    mytokens = [word for word in mytokens if word not in stopwords and word not in punctuations]
     mytokens = " ".join([i for i in mytokens])
     return mytokens
 
-
+# Function to extract selected topics from a given model
 def selected_topics(model, vectorizer, top_n=10):
     for idx, topic in enumerate(model.components_):
         print("Topic %d:" % (idx))
         print([(vectorizer.get_feature_names()[i], topic[i])
-                        for i in topic.argsort()[:-top_n - 1:-1]]) 
+              for i in topic.argsort()[:-top_n - 1:-1]])
 
-
+# Extract all entities (personal names and locations) from a given text
 entities = []
-for text in nlp.pipe(iter(bestdf['Description']), batch_size = 1000, n_threads=-1):  
-  entities.append([(X.text, X.label_) for X in text.ents])
-  print([(X.text, X.label_) for X in text.ents])
+for text in nlp.pipe(iter(bestdf['Description']), batch_size=1000, n_threads=-1):
+    entities.append([(X.text, X.label_) for X in text.ents])
+    print([(X.text, X.label_) for X in text.ents])
 
+# Create a DataFrame to store all extracted entities
+index = range(0, 13911)
+full_df = pd.DataFrame(index=index, columns=['DOC NR','CARDINAL', 'ORDINAL', 'QUANTITY', 'MONEY', 'PERCENT', 'TIME', 'DATE', 'LANGUAGE', 'LAW', 'WORK_OF_ART', 'EVENT', 'PRODUCT', 'LOC', 'GPE', 'ORG', 'FAC', 'NORP', 'PERSON'])
 
-index=range(0,13911)
-full_df = pd.DataFrame(index = index, columns=['DOC NR','CARDINAL', 'ORDINAL', 'QUANTITY', 'MONEY', 'PERCENT', 'TIME', 'DATE', 'LANGUAGE', 'LAW', 'WORK_OF_ART', 'EVENT', 'PRODUCT', 'LOC', 'GPE', 'ORG', 'FAC', 'NORP', 'PERSON'])
-
-
+# Add each extracted entity to the DataFrame
 for index, doc in enumerate(entities):
     for entity, entity_type in doc:
         if pd.isna(full_df.at[index, entity_type]):
