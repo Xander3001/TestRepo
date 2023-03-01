8ceafc509e1272ab9a5e4982cf5286ae47a78ded
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-01 22:01:44 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -1,7 +1,12 @@
+# This function calculates the Fibonacci sequence number for a given integer positive number.
+
 def fib(num):
+    # If the given number is 0 or 1, return the number.
     if num <= 1:
         return num
     else:
+        # Otherwise, return the sum of the two previous Fibonacci numbers.
         return fib(num-1) + fib(num-2)
-print(fib(10))
 
+# Print the 10th Fibonacci number.
+print(fib(10))
