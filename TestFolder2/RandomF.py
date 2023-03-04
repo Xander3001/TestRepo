75986a5bb72c5ad1e828c6becd558bf3624e071f
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-04 13:41:13 +0000

    Adding code comments

--- TestFolder2/RandomF.py
+++ TestFolder2/RandomF.py
@@ -1,15 +1,19 @@
+The overall code takes a dataset and performs various text processing and analysis tasks on it using the natural language processing library spaCy.
 
+- The first block extracts the most common nouns and verbs from the text.
+- The second block defines functions for redacting person and location names from text, and a tokenizer function.
+- The third block uses the spaCy library to extract named entities from the text and stores them in a new dataframe.
 
+# Extract most common nouns and verbs
 nouns = [ token.text for token in doc if token.is_stop != True and token.is_punct !=True and token.pos_ == 'NOUN']
 word_freq = Counter(nouns)
 common_nouns = word_freq.most_common(10)
 print(common_nouns)
 
-
 verbs = [ token.text for token in doc if token.is_punct !=True and token.pos_ == 'VERB']
 print(Counter(verbs).most_common(10))
 
-
+# Function to redact person names from text
 def sanitize_names(text):
     doc = nlp(text)
     redacted_sentences = []
@@ -21,9 +25,8 @@ def sanitize_names(text):
         else:
             redacted_sentences.append(token.string)
     return "".join(redacted_sentences)
-  
-  
 
+# Function to redact location names from text
 def sanitize_locations(text):
     doc = nlp(text)
     redacted_sentences = []
@@ -35,10 +38,8 @@ def sanitize_locations(text):
         else:
             redacted_sentences.append(token.string)
     return "".join(redacted_sentences)
-  
-  
-  
-  
+
+# Tokenizer function using spaCy
 def spacy_tokenizer(sentence):
     mytokens = parser(sentence)
     mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
@@ -46,23 +47,21 @@ def spacy_tokenizer(sentence):
     mytokens = " ".join([i for i in mytokens])
     return mytokens
 
-
+# Function to view the top topics from a topic modelling model
 def selected_topics(model, vectorizer, top_n=10):
     for idx, topic in enumerate(model.components_):
         print("Topic %d:" % (idx))
         print([(vectorizer.get_feature_names()[i], topic[i])
-                        for i in topic.argsort()[:-top_n - 1:-1]]) 
+                        for i in topic.argsort()[:-top_n - 1:-1]])
 
-
+# Extract named entities from the text and store in a new dataframe
 entities = []
 for text in nlp.pipe(iter(bestdf['Description']), batch_size = 1000, n_threads=-1):  
   entities.append([(X.text, X.label_) for X in text.ents])
   print([(X.text, X.label_) for X in text.ents])
 
-
 index=range(0,13911)
 full_df = pd.DataFrame(index = index, columns=['DOC NR','CARDINAL', 'ORDINAL', 'QUANTITY', 'MONEY', 'PERCENT', 'TIME', 'DATE', 'LANGUAGE', 'LAW', 'WORK_OF_ART', 'EVENT', 'PRODUCT', 'LOC', 'GPE', 'ORG', 'FAC', 'NORP', 'PERSON'])
-
 
 for index, doc in enumerate(entities):
     for entity, entity_type in doc:
