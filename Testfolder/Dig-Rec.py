b72b8fba2f6725fdd223e0c6c7784cd0c8d32e01
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-04 13:41:35 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -1,7 +1,16 @@
+# The code is an implementation of the Fibonacci sequence
+# It takes a number as input and returns the corresponding Fibonacci number
+
 def fib(num):
+    """
+    Compute the Fibonacci of given number
+    
+    :param num: int, input number to compute Fibonacci of
+    :return: int, Fibonacci number of input 
+    """
     if num <= 1:
         return num
     else:
         return fib(num-1) + fib(num-2)
-print(fib(10))
 
+print(fib(10)) # prints the 10th Fibonacci number
