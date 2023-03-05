02e22633b5c51cb040b9b9b2083f7fc2a4da25a5
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 15:09:18 +0000

    Adding code comments

--- Testfolder/Digit_Recognizer_DL.py
+++ Testfolder/Digit_Recognizer_DL.py
@@ -1,35 +1,31 @@
-import numpy as np
-import matplotlib.pyplot as plt
+Overall code description:
 
+This code contains functions for implementing the softmax and ReLU activation functions used in deep learning models. It also includes their respective backward propagation functions for computing gradients during backpropagation.
 
+# softmax function: computes the softmax activation for input vector z
 def softmax(z):
     cache = z
-    z -= np.max(z)
-    sm = (np.exp(z).T / np.sum(np.exp(z), axis=1))
+    z -= np.max(z) # Subtracting max value for numerical stability
+    sm = (np.exp(z).T / np.sum(np.exp(z), axis=1)) # Softmax computation
     return sm, cache
 
-
+# ReLU function: computes the rectified linear unit activation for input vector z
 def relu(z):
-
-    s = np.maximum(0, z)
+    s = np.maximum(0, z) # ReLU computation
     cache = z
     return s, cache
 
-
+# softmax_backward function: performs the backward pass for softmax function during backpropagation
 def softmax_backward(dA, cache):
-
     z = cache
-    z -= np.max(z)
-    s = (np.exp(z).T / np.sum(np.exp(z), axis=1))
-    dZ = dA * s * (1 - s)
+    z -= np.max(z) # Subtracting max value for numerical stability
+    s = (np.exp(z).T / np.sum(np.exp(z), axis=1)) # Softmax computation
+    dZ = dA * s * (1 - s) # Derivative of softmax function
     return dZ
 
-
+# relu_backward function: performs the backward pass for ReLU function during backpropagation
 def relu_backward(dA, cache):
- 
     Z = cache
-    dZ = np.array(dA, copy=True)  # just converting dz to a correct object.
-    dZ[Z <= 0] = 0
+    dZ = np.array(dA, copy=True)
+    dZ[Z <= 0] = 0 # Derivative of ReLU function
     return dZ
-
-
