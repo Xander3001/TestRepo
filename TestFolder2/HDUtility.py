9cded6ae4a1484eaff9ee7090a1051f4bac0ebea
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 11:26:38 +0000

    Adding code comments

--- TestFolder2/HDUtility.py
+++ TestFolder2/HDUtility.py
@@ -1,8 +1,22 @@
+# This code defines two functions that are used to explore a given path and list all the files within it.
+# The exploreTree function recursively explores the specified path and appends any files it finds to a list.
+# The listFiles function uses the exploreTree function to create a list of all files within the specified path and returns a Spark dataframe with metadata for each file.
+
 
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
 def exploreTree(blobFiles,path):
+  """
+  Recursively explores the specified path and appends any files it finds to a list.
+
+  Parameters:
+  blobFiles (list): list of file paths
+  path (str): path to explore
+
+  Returns:
+  list: updated list of file paths
+  """
   for ent in dbutils.fs.ls(path):
     if (ent.path[-1:]) == "/":
       #print(f"exploring: {ent.path}")
@@ -13,6 +27,16 @@ def exploreTree(blobFiles,path):
   return blobFiles
 
 def listFiles(path):
+  """
+  Uses the exploreTree function to create a list of all files within the specified path and returns
+  a Spark dataframe with metadata for each file.
+
+  Parameters:
+  path (str): path to explore for files
+
+  Returns:
+  DataFrame: Spark dataframe with metadata for each file.
+  """
   blobFiles = []
   exploreTree(blobFiles,path)
   
