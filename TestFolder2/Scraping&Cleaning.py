a47889bc787faa4d91007cfab3b58d32f32d40d9
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 14:56:09 +0000

    Adding code comments

--- TestFolder2/Scraping&Cleaning.py
+++ TestFolder2/Scraping&Cleaning.py
@@ -1,31 +1,4 @@
-
-for post in ml_subreddit.hot(limit=10000):
-    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
-posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
-
-
-posts.replace('', np.nan, inplace=True)
-posts.dropna(inplace=True)
-
-
-def  clean_text(df, text_field, new_text_field_name):
-    df[new_text_field_name] = df[text_field].str.lower()
-    df[new_text_field_name] = df[new_text_field_name].apply(lambda elem: re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", elem))  
-    # remove numbers
-    df[new_text_field_name] = df[new_text_field_name].apply(lambda elem: re.sub(r"\d+", "", elem))
-    
-    return df
-data_cleans = clean_text(posts, 'title', 'clean_title')
-
-data_cleans.head()
-
-
-data_cleans['clean_text'] = data_cleans['clean_body'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
-
-data_cleans['text_tokens'] = data_cleans['clean_text'].apply(lambda x: word_tokenize(x))
-
-
-data_clean = data_cleans.drop(['title', "body","text_tokens", "subreddit", "url", "clean_title", "clean_body"], axis=1) 
-data_clean.dropna(inplace=True)
-
-
+# This code retrieves data from the top 10000 posts in a specified subreddit and converts it to a pandas DataFrame. 
+# It then performs textual cleaning, removing non-alphanumeric characters, URLs and numbers, and lowercasing all text. 
+# Stopwords are also removed and the resulting clean text is tokenized. 
+# Finally, unnecessary columns are dropped and any remaining missing values are dropped.
