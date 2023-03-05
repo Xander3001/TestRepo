8ccdc3839c8c8234fd8e1bd4d2db16db68414c43
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 15:24:29 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -1,7 +1,13 @@
+# This function calculates the nth term of the Fibonacci sequence recursively
+# Parameter: num - integer representing the nth term of Fibonacci sequence to calculate
+# Returns: integer representing the value of the nth term of the Fibonacci sequence
 def fib(num):
+    # Base case when num is 0 or 1
     if num <= 1:
         return num
     else:
+        # Recursive call to calculate the Fibonacci sequence
         return fib(num-1) + fib(num-2)
-print(fib(10))
 
+# Prints the 10th term of the Fibonacci sequence
+print(fib(10))
