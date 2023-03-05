cdb999e405af79ea6d8d406b474f8164bf27ab99
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 15:17:08 +0000

    Adding code comments

--- TestFolder2/RandomF.py
+++ TestFolder2/RandomF.py
@@ -1,15 +1,22 @@
-
-
+# Create a list of nouns from the "doc" object
 nouns = [ token.text for token in doc if token.is_stop != True and token.is_punct !=True and token.pos_ == 'NOUN']
+
+# Count the frequency of each noun and get the 10 most common
 word_freq = Counter(nouns)
 common_nouns = word_freq.most_common(10)
+
+# Print the 10 most common nouns
 print(common_nouns)
 
 
+# Create a list of verbs from the "doc" object
 verbs = [ token.text for token in doc if token.is_punct !=True and token.pos_ == 'VERB']
+
+# Count the frequency of each verb and get the 10 most common
 print(Counter(verbs).most_common(10))
 
 
+# Define a function to redact PERSON entities in text
 def sanitize_names(text):
     doc = nlp(text)
     redacted_sentences = []
@@ -21,9 +28,9 @@ def sanitize_names(text):
         else:
             redacted_sentences.append(token.string)
     return "".join(redacted_sentences)
-  
-  
 
+
+# Define a function to redact GPE entities in text
 def sanitize_locations(text):
     doc = nlp(text)
     redacted_sentences = []
@@ -35,10 +42,9 @@ def sanitize_locations(text):
         else:
             redacted_sentences.append(token.string)
     return "".join(redacted_sentences)
-  
-  
-  
-  
+
+
+# Define a tokenizer function using SpaCy
 def spacy_tokenizer(sentence):
     mytokens = parser(sentence)
     mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
@@ -47,6 +53,7 @@ def spacy_tokenizer(sentence):
     return mytokens
 
 
+# Define a function to print the top features for each topic in an LDA model
 def selected_topics(model, vectorizer, top_n=10):
     for idx, topic in enumerate(model.components_):
         print("Topic %d:" % (idx))
@@ -54,16 +61,18 @@ def selected_topics(model, vectorizer, top_n=10):
                         for i in topic.argsort()[:-top_n - 1:-1]]) 
 
 
+# Generate a list of entities in each text from the "Description" column of a pandas DataFrame
 entities = []
 for text in nlp.pipe(iter(bestdf['Description']), batch_size = 1000, n_threads=-1):  
   entities.append([(X.text, X.label_) for X in text.ents])
   print([(X.text, X.label_) for X in text.ents])
 
 
+# Initialize an empty pandas DataFrame with columns for each entity type
 index=range(0,13911)
 full_df = pd.DataFrame(index = index, columns=['DOC NR','CARDINAL', 'ORDINAL', 'QUANTITY', 'MONEY', 'PERCENT', 'TIME', 'DATE', 'LANGUAGE', 'LAW', 'WORK_OF_ART', 'EVENT', 'PRODUCT', 'LOC', 'GPE', 'ORG', 'FAC', 'NORP', 'PERSON'])
 
-
+# Iterate over each set of entities and add them to the appropriate column in full_df
 for index, doc in enumerate(entities):
     for entity, entity_type in doc:
         if pd.isna(full_df.at[index, entity_type]):
