ad223d1473cfb0015b9fb81cac8fdca53fbf5c3b
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 15:08:40 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -1,7 +1,23 @@
+# The code implementation below is the recursive implementation of the Fibonacci sequence. 
+
 def fib(num):
+    """
+    A function that generates the nth number in the Fibonacci sequence recursively
+
+    Parameters:
+    num(int): A non-negative integer representing the index of the desired number in the sequence
+
+    Returns:
+    int: The nth number in the Fibonacci sequence.
+ 
+    """
     if num <= 1:
         return num
     else:
         return fib(num-1) + fib(num-2)
-print(fib(10))
 
+print(fib(10))  # Expected output: 55
+
+# The function accepts an integer argument representing the nth number in the Fibonacci sequence, 
+# then returns that number using a recursive implementation. This specific implementation filters 
+# numbers less than <= 1 and calculates the previous two Fibonacci numbers and sums them up to get the desired nth number.
