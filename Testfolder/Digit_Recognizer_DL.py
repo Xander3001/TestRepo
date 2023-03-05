1b9c2707333e4e497a16efc1cee759c9cb129de2
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 20:42:14 +0000

    Adding code comments

--- Testfolder/Digit_Recognizer_DL.py
+++ Testfolder/Digit_Recognizer_DL.py
@@ -3,6 +3,15 @@ import matplotlib.pyplot as plt
 
 
 def softmax(z):
+    """
+    Compute the softmax activation function for a given input vector.
+
+    Args:
+        z (numpy.ndarray): Input vector.
+
+    Returns:
+        Tuple containing the softmax output and the input vector itself, saved for backpropagation purposes.
+    """
     cache = z
     z -= np.max(z)
     sm = (np.exp(z).T / np.sum(np.exp(z), axis=1))
@@ -10,14 +19,31 @@ def softmax(z):
 
 
 def relu(z):
+    """
+    Compute the ReLU activation function for a given input vector.
 
+    Args:
+        z (numpy.ndarray): Input vector.
+
+    Returns:
+        Tuple containing the ReLU output and the input vector itself, saved for backpropagation purposes.
+    """
     s = np.maximum(0, z)
     cache = z
     return s, cache
 
 
 def softmax_backward(dA, cache):
+    """
+    Compute the derivative of the softmax activation function.
 
+    Args:
+        dA (numpy.ndarray): Gradient of the loss function with respect to the softmax output.
+        cache (numpy.ndarray): Input vector.
+
+    Returns:
+        Gradient of the loss function with respect to the input vector.
+    """
     z = cache
     z -= np.max(z)
     s = (np.exp(z).T / np.sum(np.exp(z), axis=1))
@@ -26,10 +52,17 @@ def softmax_backward(dA, cache):
 
 
 def relu_backward(dA, cache):
- 
+    """
+    Compute the derivative of the ReLU activation function.
+
+    Args:
+        dA (numpy.ndarray): Gradient of the loss function with respect to the ReLU output.
+        cache (numpy.ndarray): Input vector.
+
+    Returns:
+        Gradient of the loss function with respect to the input vector.
+    """
     Z = cache
     dZ = np.array(dA, copy=True)  # just converting dz to a correct object.
     dZ[Z <= 0] = 0
     return dZ
-
-
