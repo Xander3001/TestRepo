79d6f5d17ae6c994af8fa42f0224a3a3e0dc6f8a
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 15:24:52 +0000

    Adding code comments

--- Testfolder/Digit-Recognizer.py
+++ Testfolder/Digit-Recognizer.py
@@ -1,35 +1,62 @@
+# The while loop iterates as long as the object 'cap' is open.
+while (cap.isOpened()):
+    
+    # Reads a frame from the input video.
+    ret, img = cap.read()
+    
+    # Gets the image, contours and threshold values for the image.
+    img, contours, thresh = get_img_contour_thresh(img)
+    
+    # Initializes empty strings for the answer for each prediction model.
+    ans1 = ''
+    ans2 = ''
+    ans3 = ''
+    
+    # If there are contours detected in the image.
+    if len(contours) > 0:
+        
+        # Gets the contour with the largest area.
+        contour = max(contours, key=cv2.contourArea)
+        
+        # If the area is greater than 2500, it predicts the digit.
+        if cv2.contourArea(contour) > 2500:
+            
+            # Gets the x, y, width, and height values of the bounding rectangle of the contour.
+            x, y, w, h = cv2.boundingRect(contour)
+            
+            # Gets a new image of the digit to be predicted.
+            newImage = thresh[y:y + h, x:x + w]
+            
+            # Resizes the new image to 28x28 and flattens it.
+            newImage = cv2.resize(newImage, (28, 28))
+            newImage = np.array(newImage)
+            newImage = newImage.flatten()
+            newImage = newImage.reshape(newImage.shape[0], 1)
+            
+            # Predicts the digit using Logistic Regression model.
+            ans1 = Digit_Recognizer_LR.predict(w_LR, b_LR, newImage)
+            
+            # Predicts the digit using Shallow Network model.
+            ans2 = Digit_Recognizer_NN.predict_nn(d2, newImage)
+            
+            # Predicts the digit using Deep Network model.
+            ans3 = Digit_Recognizer_DL.predict(d3, newImage)
 
-    while (cap.isOpened()):
-        ret, img = cap.read()
-        img, contours, thresh = get_img_contour_thresh(img)
-        ans1 = ''
-        ans2 = ''
-        ans3 = ''
-        if len(contours) > 0:
-            contour = max(contours, key=cv2.contourArea)
-            if cv2.contourArea(contour) > 2500:
-                # print(predict(w_from_model,b_from_model,contour))
-                x, y, w, h = cv2.boundingRect(contour)
-                # newImage = thresh[y - 15:y + h + 15, x - 15:x + w +15]
-                newImage = thresh[y:y + h, x:x + w]
-                newImage = cv2.resize(newImage, (28, 28))
-                newImage = np.array(newImage)
-                newImage = newImage.flatten()
-                newImage = newImage.reshape(newImage.shape[0], 1)
-                ans1 = Digit_Recognizer_LR.predict(w_LR, b_LR, newImage)
-                ans2 = Digit_Recognizer_NN.predict_nn(d2, newImage)
-                ans3 = Digit_Recognizer_DL.predict(d3, newImage)
-
-        x, y, w, h = 0, 0, 300, 300
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(img, "Logistic Regression : " + str(ans1), (10, 320),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(img, "Shallow Network :  " + str(ans2), (10, 350),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(img, "Deep Network :  " + str(ans3), (10, 380),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.imshow("Frame", img)
-        cv2.imshow("Contours", thresh)
-        k = cv2.waitKey(10)
-        if k == 27:
-            break
+    # Draws a rectangle around the original image and puts the predicted digit values for each model on the frame.
+    x, y, w, h = 0, 0, 300, 300
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    cv2.putText(img, "Logistic Regression : " + str(ans1), (10, 320),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(img, "Shallow Network :  " + str(ans2), (10, 350),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(img, "Deep Network :  " + str(ans3), (10, 380),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    
+    # Shows the frame with predictions and the thresholded image.
+    cv2.imshow("Frame", img)
+    cv2.imshow("Contours", thresh)
+    
+    # Waits for a key input to exit the program.
+    k = cv2.waitKey(10)
+    if k == 27:
+        break
