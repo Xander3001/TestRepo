56ed112dc0da623a5a605aee271c376a37a1b6ca
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 15:17:38 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -1,7 +1,9 @@
-def fib(num):
-    if num <= 1:
+# This function calculates the fibonacci sequence up to a given number "num"
+# and returns the value at that number's index in the sequence.
+def fib(num): 
+    if num <= 1: # If input number is 0 or 1, return that number.
         return num
-    else:
+    else: # If input number is greater than 1, calculate the fibonacci sequence recursively
         return fib(num-1) + fib(num-2)
-print(fib(10))
-
+    
+print(fib(10)) # Call the function and print the result
