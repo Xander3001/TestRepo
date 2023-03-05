15d64f06dc16d7912531942c6a1c202d7c953fc2
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 20:28:42 +0000

    Adding code comments

--- TestFolder2/RandomF.py
+++ TestFolder2/RandomF.py
@@ -1,4 +1,4 @@
-
+# The following code creates a list of nouns from the text and counts their frequency
 
 nouns = [ token.text for token in doc if token.is_stop != True and token.is_punct !=True and token.pos_ == 'NOUN']
 word_freq = Counter(nouns)
@@ -6,9 +6,13 @@ common_nouns = word_freq.most_common(10)
 print(common_nouns)
 
 
+# The following code creates a list of verbs from the text and counts their frequency
+
 verbs = [ token.text for token in doc if token.is_punct !=True and token.pos_ == 'VERB']
 print(Counter(verbs).most_common(10))
 
+
+# The following function replaces all PERSON entities with the string "[REDACTED]"
 
 def sanitize_names(text):
     doc = nlp(text)
@@ -22,7 +26,8 @@ def sanitize_names(text):
             redacted_sentences.append(token.string)
     return "".join(redacted_sentences)
   
-  
+
+# The following function replaces all GPE entities with the string "[REDACTED]"
 
 def sanitize_locations(text):
     doc = nlp(text)
@@ -37,8 +42,8 @@ def sanitize_locations(text):
     return "".join(redacted_sentences)
   
   
-  
-  
+# The following function tokenizes a sentence using spaCy, lemmatizes the words, removes stop words and punctuation, and returns the processed sentence as a string. 
+
 def spacy_tokenizer(sentence):
     mytokens = parser(sentence)
     mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
@@ -47,6 +52,8 @@ def spacy_tokenizer(sentence):
     return mytokens
 
 
+# The following function prints the top topics from a given model and vectorizer using the topic modeling technique.
+
 def selected_topics(model, vectorizer, top_n=10):
     for idx, topic in enumerate(model.components_):
         print("Topic %d:" % (idx))
@@ -54,15 +61,18 @@ def selected_topics(model, vectorizer, top_n=10):
                         for i in topic.argsort()[:-top_n - 1:-1]]) 
 
 
+# The following code identifies entities in the text and creates a dataframe to store them by type.
+
 entities = []
 for text in nlp.pipe(iter(bestdf['Description']), batch_size = 1000, n_threads=-1):  
   entities.append([(X.text, X.label_) for X in text.ents])
   print([(X.text, X.label_) for X in text.ents])
 
-
 index=range(0,13911)
 full_df = pd.DataFrame(index = index, columns=['DOC NR','CARDINAL', 'ORDINAL', 'QUANTITY', 'MONEY', 'PERCENT', 'TIME', 'DATE', 'LANGUAGE', 'LAW', 'WORK_OF_ART', 'EVENT', 'PRODUCT', 'LOC', 'GPE', 'ORG', 'FAC', 'NORP', 'PERSON'])
 
+
+# The following code populates the dataframe with entities by type.
 
 for index, doc in enumerate(entities):
     for entity, entity_type in doc:
