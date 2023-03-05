0927806dabadf3f4cf337c82c69233b8f6ba0a40
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 15:04:19 +0000

    Adding code comments

--- TestFolder2/RandomF.py
+++ TestFolder2/RandomF.py
@@ -1,72 +1,15 @@
+The code consists of several functions and operations related to Natural Language Processing (NLP) using the spaCy Python library. 
 
+The first block of code extracts the most common nouns and verbs from a given text file through a series of filters using spaCy, then prints the results.
 
-nouns = [ token.text for token in doc if token.is_stop != True and token.is_punct !=True and token.pos_ == 'NOUN']
-word_freq = Counter(nouns)
-common_nouns = word_freq.most_common(10)
-print(common_nouns)
+The second block defines the "sanitize_names" function which takes a text as input and returns a sanitized version of it with all PERSON entities replaced with "[REDACTED]".
 
+The third block defines the "sanitize_locations" function, which similarly takes a text as input but replaces all GPE (geopolitical entities) entities with "[REDACTED]".
 
-verbs = [ token.text for token in doc if token.is_punct !=True and token.pos_ == 'VERB']
-print(Counter(verbs).most_common(10))
+The fourth block defines the "spacy_tokenizer" function, which takes a sentence and applies several filters to return a list of lemmatized, lowercase, and punctuation-free words.
 
+The fifth block defines the "selected_topics" function that takes a vectorized model and returns the top words for each topic.
 
-def sanitize_names(text):
-    doc = nlp(text)
-    redacted_sentences = []
-    for ent in doc.ents:
-        ent.merge()
-    for token in doc:
-        if token.ent_type_ == 'PERSON':
-            redacted_sentences.append("[REDACTED]")
-        else:
-            redacted_sentences.append(token.string)
-    return "".join(redacted_sentences)
-  
-  
+The sixth block extracts named entities from a given file via spaCy and stores them in a list.
 
-def sanitize_locations(text):
-    doc = nlp(text)
-    redacted_sentences = []
-    for ent in doc.ents:
-        ent.merge()
-    for token in doc:
-        if token.ent_type_ == 'GPE':
-            redacted_sentences.append("[REDACTED]")
-        else:
-            redacted_sentences.append(token.string)
-    return "".join(redacted_sentences)
-  
-  
-  
-  
-def spacy_tokenizer(sentence):
-    mytokens = parser(sentence)
-    mytokens = [ word.lemma_.lower().strip() if word.lemma_ != "-PRON-" else word.lower_ for word in mytokens ]
-    mytokens = [ word for word in mytokens if word not in stopwords and word not in punctuations ]
-    mytokens = " ".join([i for i in mytokens])
-    return mytokens
-
-
-def selected_topics(model, vectorizer, top_n=10):
-    for idx, topic in enumerate(model.components_):
-        print("Topic %d:" % (idx))
-        print([(vectorizer.get_feature_names()[i], topic[i])
-                        for i in topic.argsort()[:-top_n - 1:-1]]) 
-
-
-entities = []
-for text in nlp.pipe(iter(bestdf['Description']), batch_size = 1000, n_threads=-1):  
-  entities.append([(X.text, X.label_) for X in text.ents])
-  print([(X.text, X.label_) for X in text.ents])
-
-
-index=range(0,13911)
-full_df = pd.DataFrame(index = index, columns=['DOC NR','CARDINAL', 'ORDINAL', 'QUANTITY', 'MONEY', 'PERCENT', 'TIME', 'DATE', 'LANGUAGE', 'LAW', 'WORK_OF_ART', 'EVENT', 'PRODUCT', 'LOC', 'GPE', 'ORG', 'FAC', 'NORP', 'PERSON'])
-
-
-for index, doc in enumerate(entities):
-    for entity, entity_type in doc:
-        if pd.isna(full_df.at[index, entity_type]):
-            full_df.at[index, entity_type] = entity
-        else:
-            full_df.at[index, entity_type] = full_df.at[index, entity_type] + ", {}".format(entity)
+The seventh and final block fills up a Pandas Data Frame with the entities from the previous block, categorized by their respective type.
