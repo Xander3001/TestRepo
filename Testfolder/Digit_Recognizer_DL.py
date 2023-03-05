f7166b9b977875e2352ab1148480533b0be1d5cb
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 15:29:50 +0000

    Adding code comments

--- Testfolder/Digit_Recognizer_DL.py
+++ Testfolder/Digit_Recognizer_DL.py
@@ -1,35 +1,35 @@
+# Importing necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 
 
+# This function returns the softmax activation of input 'z' and stores the original input in 'cache'
 def softmax(z):
     cache = z
-    z -= np.max(z)
-    sm = (np.exp(z).T / np.sum(np.exp(z), axis=1))
+    z -= np.max(z)  # subtracting maximum value of 'z' for numerical stability
+    sm = (np.exp(z).T / np.sum(np.exp(z), axis=1))  # softmax activation applied to 'z'
     return sm, cache
 
 
+# This function returns the relu activation of input 'z' and stores the original input in 'cache'
 def relu(z):
-
-    s = np.maximum(0, z)
+    s = np.maximum(0, z)  # relu activation applied to 'z'
     cache = z
     return s, cache
 
 
+# This function returns the derivative of softmax activation given the output dA and original input 'cache'
 def softmax_backward(dA, cache):
-
     z = cache
-    z -= np.max(z)
-    s = (np.exp(z).T / np.sum(np.exp(z), axis=1))
-    dZ = dA * s * (1 - s)
+    z -= np.max(z)  # subtracting maximum value of 'z' for numerical stability
+    s = (np.exp(z).T / np.sum(np.exp(z), axis=1))  # softmax activation applied to 'z'
+    dZ = dA * s * (1 - s)  # element-wise multiplication of dA and derivative of softmax activation
     return dZ
 
 
+# This function returns the derivative of relu activation given the output dA and original input 'cache'
 def relu_backward(dA, cache):
- 
     Z = cache
-    dZ = np.array(dA, copy=True)  # just converting dz to a correct object.
-    dZ[Z <= 0] = 0
+    dZ = np.array(dA, copy=True)  # creating a copy of dA
+    dZ[Z <= 0] = 0  # applying derivative of relu activation to dZ
     return dZ
-
-
