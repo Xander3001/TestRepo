49c2ed1a7fa26b68cc7336d62074638aab887a60
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-05 20:29:29 +0000

    Adding code comments

--- Testfolder/Digit-Recognizer.py
+++ Testfolder/Digit-Recognizer.py
@@ -1,35 +1,59 @@
+# Open camera and capture video
+while (cap.isOpened()):
+    # Read the image frame from captured video
+    ret, img = cap.read()
 
-    while (cap.isOpened()):
-        ret, img = cap.read()
-        img, contours, thresh = get_img_contour_thresh(img)
-        ans1 = ''
-        ans2 = ''
-        ans3 = ''
-        if len(contours) > 0:
-            contour = max(contours, key=cv2.contourArea)
-            if cv2.contourArea(contour) > 2500:
-                # print(predict(w_from_model,b_from_model,contour))
-                x, y, w, h = cv2.boundingRect(contour)
-                # newImage = thresh[y - 15:y + h + 15, x - 15:x + w +15]
-                newImage = thresh[y:y + h, x:x + w]
-                newImage = cv2.resize(newImage, (28, 28))
-                newImage = np.array(newImage)
-                newImage = newImage.flatten()
-                newImage = newImage.reshape(newImage.shape[0], 1)
-                ans1 = Digit_Recognizer_LR.predict(w_LR, b_LR, newImage)
-                ans2 = Digit_Recognizer_NN.predict_nn(d2, newImage)
-                ans3 = Digit_Recognizer_DL.predict(d3, newImage)
+    # apply filter and get thresholded image
+    img, contours, thresh = get_img_contour_thresh(img)
 
-        x, y, w, h = 0, 0, 300, 300
-        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        cv2.putText(img, "Logistic Regression : " + str(ans1), (10, 320),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(img, "Shallow Network :  " + str(ans2), (10, 350),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.putText(img, "Deep Network :  " + str(ans3), (10, 380),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        cv2.imshow("Frame", img)
-        cv2.imshow("Contours", thresh)
-        k = cv2.waitKey(10)
-        if k == 27:
-            break
+    # initialize variables for storing predicted values
+    ans1 = ''
+    ans2 = ''
+    ans3 = ''
+
+    # check if contours are found in image
+    if len(contours) > 0:
+        # select the largest contour
+        contour = max(contours, key=cv2.contourArea)
+        # check if contour size is larger than 2500
+        if cv2.contourArea(contour) > 2500:
+            # get the dimensions of bounding rectangle
+            x, y, w, h = cv2.boundingRect(contour)
+            # crop the image to area containing number
+            newImage = thresh[y:y + h, x:x + w]
+            # resize cropped image to 28 x 28 pixels
+            newImage = cv2.resize(newImage, (28, 28))
+            # convert cropped image into an array
+            newImage = np.array(newImage)
+            # flatten the image into a single row
+            newImage = newImage.flatten()
+            # reshape the image
+            newImage = newImage.reshape(newImage.shape[0], 1)
+            # predict number using logistic regression model
+            ans1 = Digit_Recognizer_LR.predict(w_LR, b_LR, newImage)
+            # predict number using shallow neural network model
+            ans2 = Digit_Recognizer_NN.predict_nn(d2, newImage)
+            # predict number using deep neural network model
+            ans3 = Digit_Recognizer_DL.predict(d3, newImage)
+
+    # set the dimensions of rectangle to be drawn on image frame
+    x, y, w, h = 0, 0, 300, 300
+    # draw rectangle on image frame
+    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # write predicted numbers on image
+    cv2.putText(img, "Logistic Regression : " + str(ans1), (10, 320),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(img, "Shallow Network :  " + str(ans2), (10, 350),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.putText(img, "Deep Network :  " + str(ans3), (10, 380),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+    # display the image frame and thresholded image
+    cv2.imshow("Frame", img)
+    cv2.imshow("Contours", thresh)
+
+    # wait for key press event
+    k = cv2.waitKey(10)
+    # if ESC key is pressed, exit the loop and stop capturing
+    if k == 27:
+        break
