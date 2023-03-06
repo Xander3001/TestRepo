81edf826d48fb9e627ac37d826adcf651cd733fa
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-06 12:01:10 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -1,7 +1,9 @@
-def fib(num):
+def fib(num): # function to calculate fibonacci series
+    '''
+    This function generates the fibonacci series of a given number.
+    '''
     if num <= 1:
         return num
     else:
         return fib(num-1) + fib(num-2)
-print(fib(10))
-
+print(fib(10)) # prints the fibonacci series of the number 10.
