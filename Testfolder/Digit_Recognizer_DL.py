8ae11f5714604825c26c0c3a051084252d126b5e
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-06 12:01:55 +0000

    Adding code comments

--- Testfolder/Digit_Recognizer_DL.py
+++ Testfolder/Digit_Recognizer_DL.py
@@ -3,6 +3,17 @@ import matplotlib.pyplot as plt
 
 
 def softmax(z):
+    """
+    Computes the softmax activation function.
+
+    Arguments:
+    z -- numpy array of any shape
+
+    Returns:
+    sm -- output of softmax(z), same shape as z
+    cache -- input to the softmax function (useful during backpropagation)
+    """
+
     cache = z
     z -= np.max(z)
     sm = (np.exp(z).T / np.sum(np.exp(z), axis=1))
@@ -10,6 +21,16 @@ def softmax(z):
 
 
 def relu(z):
+    """
+    Computes the ReLU activation function.
+
+    Arguments:
+    z -- numpy array of any shape
+
+    Returns:
+    s -- output of ReLU(z), same shape as z
+    cache -- input to the ReLU function (useful during backpropagation)
+    """
 
     s = np.maximum(0, z)
     cache = z
@@ -17,6 +38,16 @@ def relu(z):
 
 
 def softmax_backward(dA, cache):
+    """
+    Computes the derivative of the softmax function.
+
+    Arguments:
+    dA -- post-activation gradient, same shape as activation (sm)
+    cache -- input of the softmax function (same as output of forward propagation)
+
+    Returns:
+    dZ -- Gradient of the cost with respect to z
+    """
 
     z = cache
     z -= np.max(z)
@@ -26,10 +57,18 @@ def softmax_backward(dA, cache):
 
 
 def relu_backward(dA, cache):
- 
+    """
+    Computes the derivative of the ReLU function.
+
+    Arguments:
+    dA -- post-activation gradient, same shape as activation (s)
+    cache -- input of the ReLU function (same as output of forward propagation)
+
+    Returns:
+    dZ -- Gradient of the cost with respect to z
+    """
+
     Z = cache
     dZ = np.array(dA, copy=True)  # just converting dz to a correct object.
     dZ[Z <= 0] = 0
     return dZ
-
-
