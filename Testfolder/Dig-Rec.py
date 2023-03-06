596910be6a64c30aaf7fed420a8c68b4a9fe1e3d
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-06 12:03:50 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -1,7 +1,5 @@
-def fib(num):
-    if num <= 1:
-        return num
-    else:
-        return fib(num-1) + fib(num-2)
-print(fib(10))
+This code defines a function called 'fib' which takes a single argument, 'num'. The function recursively calculates and returns the Fibonacci number at the specified position in the sequence. 
 
+If the given input 'num' is less than or equal to 1, the function returns the value of 'num'. Otherwise, the function returns the sum of the two preceding Fibonacci numbers. 
+
+Lastly, the code calls the 'fib' function with an input of 10 and prints out the resulting value to the console, which should be the 10th Fibonacci number in the sequence.
