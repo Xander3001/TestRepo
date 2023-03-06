985a0ab790aca8787425553978ac1adc55fa280c
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-06 12:03:42 +0000

    Adding code comments

--- TestFolder2/Scraping&Cleaning.py
+++ TestFolder2/Scraping&Cleaning.py
@@ -1,31 +1,7 @@
+The code is scraping the 'hot' posts from a subreddit related to machine learning and creating a Pandas DataFrame 'posts' with the titles, scores, IDs, subreddits, URLs, number of comments, selftext, and creation times of the posts. Then, it removes any rows with empty values from this DataFrame. 
 
-for post in ml_subreddit.hot(limit=10000):
-    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
-posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+Next, there is a function called 'clean_text' that takes in a DataFrame, a text field (in this case, 'title'), and a new text field name (in this case, 'clean_title'). The function converts the text to lowercase, removes any special characters or URLs, and removes any numerical digits. This function returns the DataFrame with the new cleaned text field added.
 
+After this, the code creates a new column called 'clean_text' by removing any stop words (common words like 'the,' 'and,' 'or,' etc.) from the 'clean_body' field in the DataFrame. Then, it splits the cleaned text into individual tokens (words), creating a new column called 'text_tokens.'
 
-posts.replace('', np.nan, inplace=True)
-posts.dropna(inplace=True)
-
-
-def  clean_text(df, text_field, new_text_field_name):
-    df[new_text_field_name] = df[text_field].str.lower()
-    df[new_text_field_name] = df[new_text_field_name].apply(lambda elem: re.sub(r"(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)|^rt|http.+?", "", elem))  
-    # remove numbers
-    df[new_text_field_name] = df[new_text_field_name].apply(lambda elem: re.sub(r"\d+", "", elem))
-    
-    return df
-data_cleans = clean_text(posts, 'title', 'clean_title')
-
-data_cleans.head()
-
-
-data_cleans['clean_text'] = data_cleans['clean_body'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
-
-data_cleans['text_tokens'] = data_cleans['clean_text'].apply(lambda x: word_tokenize(x))
-
-
-data_clean = data_cleans.drop(['title', "body","text_tokens", "subreddit", "url", "clean_title", "clean_body"], axis=1) 
-data_clean.dropna(inplace=True)
-
-
+Finally, the code drops several columns from the DataFrame ('title', 'body', 'text_tokens', 'subreddit', 'url', 'clean_title', and 'clean_body') that are no longer needed and removes any rows with empty values from the resulting DataFrame.
