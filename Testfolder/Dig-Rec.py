3458de12d8f82066a040e186f7b89cdc1cf7f231
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-08 08:38:26 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -1,7 +1,18 @@
+'''
+Function: fib
+
+Arguments: num: integer value representing the position of the fibonacci sequence to be returned
+
+Returns: integer value representing the fibonacci number at the given position.
+'''
+
 def fib(num):
+    '''
+    This function returns the fibonacci number at the given position.
+    '''
     if num <= 1:
         return num
     else:
         return fib(num-1) + fib(num-2)
-print(fib(10))
 
+print(fib(10))
