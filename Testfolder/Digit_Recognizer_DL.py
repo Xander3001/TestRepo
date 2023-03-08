7514be7cf5ead0e55118f21b8b5421e0e20e906a
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-08 08:39:13 +0000

    Adding code comments

--- Testfolder/Digit_Recognizer_DL.py
+++ Testfolder/Digit_Recognizer_DL.py
@@ -1,8 +1,50 @@
+"""
+This script defines two activation functions: softmax and relu, and their respective
+backward functions softmax_backward and relu_backward. These functions can be used in
+neural network architectures.
+
+Functions:
+- softmax: computes the softmax of a given input.
+- relu: computes the rectified linear unit (ReLU) activation function of a given input.
+- softmax_backward: computes the backward pass of the softmax function.
+- relu_backward: computes the backward pass of the ReLU function.
+
+Inputs:
+- z: an array representing input data.
+
+Returns:
+- For softmax:
+    - sm: an array of same shape as input z, containing softmax values.
+    - cache: an array representing input data.
+- For relu:
+    - s: an array of same shape as input z, containing ReLU values.
+    - cache: an array representing input data.
+- For softmax_backward:
+    - dZ: an array of same shape as input dA, containing the gradient of the loss with
+    respect to z.
+    - cache: an array representing input data.
+- For relu_backward:
+    - dZ: an array of same shape as input dA, containing the gradient of the loss with
+    respect to z.
+    - cache: an array representing input data.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def softmax(z):
+    """
+    Compute the softmax of a given input.
+
+    Inputs:
+    - z: an array representing input data.
+
+    Returns:
+    - sm: an array of same shape as input z, containing softmax values.
+    - cache: an array representing input data.
+    """
+
     cache = z
     z -= np.max(z)
     sm = (np.exp(z).T / np.sum(np.exp(z), axis=1))
@@ -10,6 +52,16 @@ def softmax(z):
 
 
 def relu(z):
+    """
+    Compute the rectified linear unit (ReLU) activation function of a given input.
+
+    Inputs:
+    - z: an array representing input data.
+
+    Returns:
+    - s: an array of same shape as input z, containing ReLU values.
+    - cache: an array representing input data.
+    """
 
     s = np.maximum(0, z)
     cache = z
@@ -17,6 +69,18 @@ def relu(z):
 
 
 def softmax_backward(dA, cache):
+    """
+    Compute the backward pass of the softmax function.
+
+    Inputs:
+    - dA: an array representing the gradient of the loss with respect to output of the
+    softmax function.
+    - cache: an array representing input data.
+
+    Returns:
+    - dZ: an array of same shape as input dA, containing the gradient of the loss with
+    respect to z.
+    """
 
     z = cache
     z -= np.max(z)
@@ -26,10 +90,20 @@ def softmax_backward(dA, cache):
 
 
 def relu_backward(dA, cache):
- 
+    """
+    Compute the backward pass of the ReLU function.
+
+    Inputs:
+    - dA: an array representing the gradient of the loss with respect to output of the
+    ReLU function.
+    - cache: an array representing input data.
+
+    Returns:
+    - dZ: an array of same shape as input dA, containing the gradient of the loss with
+    respect to z.
+    """
+
     Z = cache
     dZ = np.array(dA, copy=True)  # just converting dz to a correct object.
     dZ[Z <= 0] = 0
     return dZ
-
-
