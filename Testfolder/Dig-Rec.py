80d0262edfbb11c86ed54b445ac9c2a5d9c80a1b
Xander3001 <100565653+Xander3001@users.noreply.github.com>
2023-03-08 09:22:46 +0000

    Adding code comments

--- Testfolder/Dig-Rec.py
+++ Testfolder/Dig-Rec.py
@@ -1,7 +1,13 @@
+'''
+A function that returns the nth number in the Fibonacci sequence.
+:param num: an integer representing the position of the desired number in the sequence
+:return: an integer representing the value of the number in the position requested
+'''
+
 def fib(num):
     if num <= 1:
         return num
     else:
         return fib(num-1) + fib(num-2)
-print(fib(10))
 
+print(fib(10))
